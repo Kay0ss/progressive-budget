689fe56e28500d2228f70b4195fa2e4f0065e8e1
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-11-15 21:15:34 -0600

    updated mongo connection

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+const opts = {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false}
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", opts);
 
 // routes
 app.use(require("./routes/api.js"));
